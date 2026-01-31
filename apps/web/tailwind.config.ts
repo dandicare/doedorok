d0480897ea33bdba73ae60342613d13ee5067a6e
zudydy <kjyjykwak@gmail.com>
2026-01-31 21:10:50 +0900

    feat: 폰트 추가

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,10 @@
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx}",
+  ],
+};
+
+export default config;
+
